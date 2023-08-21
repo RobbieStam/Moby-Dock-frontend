cb2b4719064c6e7775ee8b22627e3a284919ede2
Robbie Stamp <stamprobbie@gmail.com>
2023-08-21 17:23:24 +0100

    Adding Login and register pages including JS

---
+++ assets/login.js
@@ -0,0 +1,27 @@
+document.getElementById("login-form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const form = new FormData(e.target)
+
+  const options = {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: form.get("username"),
+      password: form.get("password")
+    })
+  }
+
+  const reponse = await fetch("https://moby-dock-api.onrender.com/login", options);
+  const data = await response.json();
+
+  if (response.status == 200) {
+    localStorage.setItem("token", data.token);
+    window.location.assign('search.html');
+  } else {
+    alert(data.error);
+  }
+})
