0ea6a545eae830622fbff698f1ef2bc2c4560f58
nplsam <psammerrick@gmail.com>
2023-08-24 12:57:27 +0100

    logout functionality working on all pages

--- assets/logout.js
+++ assets/logout.js
@@ -1,4 +1,20 @@
-document.getElementById("logout-btn").addEventListener("click", () => {
-    localStorage.removeItem("token"); // Clear the stored token
-    window.location.assign('index.html'); // Redirect to the login page
+document.getElementById("logout-btn").addEventListener("click", async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await fetch("http://localhost:4000/users/login", {
+        method: "DELETE",
+        headers: {
+          Authorization: token
+        }
+      });
+  
+      if (response.status === 200) {
+        localStorage.removeItem("token"); // Clear the stored token
+        window.location.assign('index.html'); // Redirect to the login page
+      } else {
+        console.error("Logout failed:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Logout error:", error.message);
+    }
   });
